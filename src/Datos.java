4f962f4da317a0d1f5c39e0a7c11c02f4b4c139a
Ayesa Marin <amarinconde@danielcastelao.org>
2023-09-29 09:57:37 +0200

    Nombramos en la clase Datos el método creado en la clase Operaciones

--- src/Datos.java
+++ src/Datos.java
@@ -1,5 +1,6 @@
 public class Datos {
     void getArea(){
+        System.out.println("Nombramos el metodo frenado añadido en la clase Operaciones");
 
     }
 }
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,5 +1,7 @@
 public class Operaciones {
     void getVelocidad(){
+    }
+    void getFrenado(){
 
     }
 }
