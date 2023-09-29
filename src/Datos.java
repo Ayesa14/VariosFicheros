d085b8d533dec7cf9794ee51926fe9f844b36f19
Ayesa Marin <amarinconde@danielcastelao.org>
2023-09-29 09:55:15 +0200

    Nombramos en la main el método Area añadido en la clase Datos

--- src/Datos.java
+++ src/Datos.java
@@ -1,2 +1,5 @@
 public class Datos {
+    void getArea(){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.printf("Main");
         System.out.println("Modificamos la main");
         System.out.println("Añadimos en operaciones el método velocidad");
+        System.out.println("Añadimos en Datos el metodo area");
     }
 }
